6b85bb614311b8a6375761d070cdec5b745cc2ba
Ayomide Jones <ayormeday@gmail.com>
2019-08-22 16:03:27 +0100

    completed mvps

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -16,28 +16,44 @@ Prototype Refactor
   // - Give persons the ability to poop.
   // - When pooping, the stomach should empty.
 
-  function Person (name, age){
-    this.name = name;
-    this.age = age;
+//   function Person (name, age){
+//     this.name = name;
+//     this.age = age;
+//     this.stomach = [];
   
-  } ;
+//   } ;
   
-  Person.prototype.greet = function (){
-    return `Hello, I am ${this.name} and I am ${this.age} years old`
-  };
+//   Person.prototype.greet = function (){
+//     return `Hello, I am ${this.name} and I am ${this.age} years old`
+//   };
   
-  Person.prototype.eatEdible = function (food){
-    stomach = [];
-    return stomach.push(food);
-  };
+//   Person.prototype.eatEdible = function (food){
+//     return this.stomach.push(food);
+//   };
   
-  Person.prototype.poop = function (){
-    return this.stomach =[];
-  };
+//   Person.prototype.poop = function (){
+//     return this.stomach;
+//   };
   
-  const test = new Person("Ayomide", 30);
   
-    
+  
+  class Person2 {
+      constructor(info){
+        this.name = info.name;
+        this.age = info.age;
+        this.stomach = [];
+      }
+      greet(){
+        return `Hello, I am ${this.name} and I am ${this.age} years old`
+      }
+      eatEdible(food){
+        return this.stomach.push(food); 
+      }
+      poop(){
+        return this.stomach; 
+      }
+  }
+const test = new Person2({name:"Ayomide", age:30});
 
   // TASK 2
 
@@ -49,26 +65,43 @@ Prototype Refactor
   // - Give cars the ability to be repaired.
   // - A repaired car can be driven again.
 
-  function Car (model_name, make){
-    this.model = model_name;
-    this.make = make;
-    this.odometer = 0;
- 
-  };
+//   function Car (model_name, make){
+//     this.model = model_name;
+//     this.make = make;
+//     this.odometer = 0; 
+//   };
 
-  Car.prototype.drive = function (distance){
-    return this.odometer += distance;
-  };
+//   Car.prototype.drive = function (distance){
+//     return this.odometer += distance;
+//   };
 
-  Car.prototype.crash = function (){
+//   Car.prototype.crash = function (){
   
-    return `i crashed at ${this.odometer} miles`
-  };
+//     return `i crashed at ${this.odometer} miles`
+//   };
 
-  Car.prototype.carRepaired = function (){
+//   Car.prototype.carRepaired = function (){
 
-  };
-  const testNow = new Car("benz", "mayback");
+//   };
+class Car {
+    constructor(model, make){
+        this.model = model;
+        this.make = make;
+        this.odometer = 0; 
+        this.carDrives = true;
+    }
+    drive(distance){
+        return this.odometer += distance;
+    }
+    crash(){
+        this.carDrives = false;
+        return `i crashed at ${this.odometer} miles`
+    }
+    carRepaired(){
+        return this.carDrives;
+    }
+}
+const testNow = new Car("benz", "mayback");
 
   // TASK 3
 
@@ -77,20 +110,29 @@ Prototype Refactor
   // - Babies should have the ability to play, which persons don't.
   // - By playing, a string is returned with some text of your choosing.
 
-  function Baby(name, age, toys){
-    Person.call(this, name, age);
-    this.toys = toys;
-  };
+//   function Baby(name, age, toys){
+//     Person.call(this, name, age);
+//     this.toys = toys;
+//   };
 
-  Baby.prototype = Object.create(Person.prototype);
+//   Baby.prototype = Object.create(Person.prototype);
 
-  Baby.prototype.play = function (){
-    return `I can play with my ${this.toys}`
-  }
+//   Baby.prototype.play = function (){
+//     return `I can play with my ${this.toys}`
+//   }
 
-  var sonny = new Baby('Tom', 4, "toy cars");
+//   var sonny = new Baby('Tom', 4, "toy cars");
 
-
+class Baby extends Person2 {
+    constructor (info, toys){
+        super(info);
+        this.toys = toys;
+    }
+    play(){
+        return `I can play with my ${this.toys}`
+    }
+}
+var sonny = new Baby({name:"Tony", age:3},"toy cars");
   // TASK 4
 
   // Use your imagination and come up with constructors that allow to build objects
